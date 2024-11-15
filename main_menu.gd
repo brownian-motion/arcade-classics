5ace7ec8d7e7d7c8b08087a352bcf91fb6e2a96f
JJ Brown <jj.brown.255@proton.me>
2024-11-15 14:20:59 -0700

    Add 'game select' menu

--- main_menu.gd
+++ main_menu.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +9,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_start_button_pressed() -> void:
+	var scene = preload("res://arcade_menu.tscn").instantiate()
+#	 TODO: animate the transition
+	get_tree().root.add_child(scene)
+	get_tree().root.remove_child(self)

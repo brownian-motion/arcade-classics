9dd2f54fb0a951d51cee3c6db20a5e9680a22115
JJ Brown <jj.brown.255@proton.me>
2024-11-15 20:02:12 -0700

    Controller and KB support for the menu

--- arcade_menu.gd
+++ arcade_menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$UI.find_child("GameSelect").get_child(0).grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,3 +24,13 @@ func _on_start_pac_man_pressed() -> void:
 	#get_tree().root.add_child(scene)
 	#get_tree().root.remove_child(self)
 	pass
+#
+#func _unhandled_input(event: InputEvent) -> void:
+	#if event.is_action("ui_select"):
+		#var focus = get_viewport().gui_get_focus_owner()
+		#if(focus != null && focus is Button):
+			#focus = (Button) focus
+			#focus.
+		#
+		#
+		

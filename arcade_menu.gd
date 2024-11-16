43d993c3d4a1302eb3a15767440b8f706e8b7121
JJ Brown <jj.brown.255@proton.me>
2024-11-15 20:30:48 -0700

    Dynamically generate game select menu

--- arcade_menu.gd
+++ arcade_menu.gd
@@ -1,36 +1,27 @@
 extends Control
 
+const games = [
+	{"id": "StartPacMan", "res": "res://pacman.tscn", "label_en": "Pac-Man"},
+	{"id": "StartBreakout", "res": "res://breakout.tscn", "label_en": "Breakout"},
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var game_select = $UI.find_child("GameSelect")
+	for child in game_select.get_children():
+		game_select.remove_child(child)
+	for game in games:
+		var id = game.id
+		var button = Button.new()
+		button.text = game.label_en
+		button.name = id
+		button.pressed.connect(func(): self.on_game_selected(game))
+		button.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		button.size_flags_vertical = Control.SIZE_EXPAND_FILL
+		game_select.add_child(button)
 	$UI.find_child("GameSelect").get_child(0).grab_focus()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_start_breakout_pressed() -> void:
-	var scene = preload("res://breakout.tscn").instantiate()
-#	 TODO: animate the transition
-	get_tree().root.add_child(scene)
-	get_tree().root.remove_child(self)
-	
-	
-func _on_start_pac_man_pressed() -> void:
-	#var scene = preload("res://pacman.tscn").instantiate()
-##	 TODO: animate the transition
-	#get_tree().root.add_child(scene)
-	#get_tree().root.remove_child(self)
-	pass
-#
-#func _unhandled_input(event: InputEvent) -> void:
-	#if event.is_action("ui_select"):
-		#var focus = get_viewport().gui_get_focus_owner()
-		#if(focus != null && focus is Button):
-			#focus = (Button) focus
-			#focus.
-		#
-		#
+func on_game_selected(game) -> void:
+	print("clicked "+game.id)
+	get_tree().change_scene_to_file(game.res)
 		
